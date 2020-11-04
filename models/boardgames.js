ae1a20927f07b88c2df1b419e7641bec59ae3db4
Erik Tomlinson <eptomlinson@gmail.com>
2020-11-04 17:26:11 -0600

    + results display, add to list button, post funct/boardgames.js to export

---
+++ models/boardgames.js
@@ -0,0 +1,12 @@
+// set up table for user input data, see below:
+// we are getting an error for Boardgames, we should ask for help on this during class
+module.exports = function(sequelize, DataTypes) {
+    var Boardgames = sequelize.define("Boardgames", {
+        Boardgames: {
+            type: DataTypes.STRING,
+            description: DataTypes.TEXT,
+            allowNull: true
+        }
+    });
+    return Boardgames;
+};
