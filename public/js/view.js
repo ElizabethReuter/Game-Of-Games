dbed4bfe0d0ff35d560be7e17f42f3dcf9d58211
Erik Tomlinson <eptomlinson@gmail.com>
2020-11-04 14:38:22 -0600

    fleshed out handlebars, added user input, params on view

--- public/js/view.js
+++ public/js/view.js
@@ -6,15 +6,33 @@ $('#gameBtn').on('click', function (data) {
     console.log("button clicked!!!");
     data.preventDefault();
     var userInputName = $('#userInputName').val();
-    console.log(userInputName);
+    // console.log(userInputName);
     var userInputYear = $('#userInputYear').val();
-    console.log(userInputYear);
+    // console.log(userInputYear);
+    // var gameQuery = "http://api.boardgameatlas.com/api/search?name=${name}&pretty=true&client_id=Yulk2IjBB9&year_published=${yearPublished}&min_players=${minPlayers}&max_players=${maxPlayers}&min_playtime=$(minPlaytime}&max_playtime=${maxPlaytime}&min_age=${minAge}";
 
-    let searchGameParms = {
-        name: "name",
-        year_published: "year",
-        min_players: "numOfPlayers"
+    let searchGameParams = {
+        name: userInputName,
+        yearPublished: userInputYear,
+        minPlayers: minNumOfPlayers,
+        maxPlayers: maxNumOfPlayers,
+        minPlaytime: minTime,
+        maxPlaytime: maxTime,
+        minAge: minAge
     }
+    console.log(searchGameParams);
+    $.ajax({
+        url: `/api/search/${JSON.stringify(searchGameParams)}`,
+        method: 'GET',
+    }).then(function(res){
+        console.log(res);
+        // if (res.length !==0){
+        // for (let i =1; i <res.length; i++){
+        //     console.log(i);
+        //     // var gameList = $("<li>");
+        // }
+        // res.render(res);
+    })
     //     var queryUrl = "https://www.boardgameatlas.com/api/search?"
     //     +userInput+
     //     "order_by=popularity&ascending=false&pretty=true&client_id=JLBr5npPhV"
@@ -29,8 +47,6 @@ $('#gameBtn').on('click', function (data) {
     //     console.log(res);
     //     $("#gameResults").empty();
     //     array = res.name;
-    //     for (let i =1; i <5; i++){
-    //         console.log(i);
-    //     }
+
     //     let gameName = res.gameName;
 })
--- routes/index.js
+++ routes/index.js
@@ -18,39 +18,23 @@ router.route('/')
         })
     });
 
-router.route('/api/searchName/:name').get(async function(req, res){
-    let gameName = req.params.name
-    let searchResult = await searchByName(gameName)
-    res.send(searchResult)
+router.route('/api/search/:data').get(async function(req, res){
+    let gameData = JSON.parse(req.params.data)
+    let searchResult = await searchByAll(gameData);
+    console.log(gameData.name);
+    res.send(searchResult);
 });
 
-router.route('/api/searchYear/:year').get(async function(req, res){
-    let gameYear = req.params.year
-    let searchResult = await searchByYear(gameYear)
-    res.send(searchResult)
-});
-
-// search by name 
-
-async function searchByName(name) {
+async function searchByAll(gameData) {
     try {
         let clientId = "Yulk2IjBB9"
-        let {data}= await axios.get(`https://api.boardgameatlas.com/api/search?name=${name}&pretty=true&client_id=${clientId}`);
+        let {name, yearPublished, minPlayers, maxPlayers, minPlaytime, maxPlaytime, minAge} = gameData;
+        let {data}= await axios.get(`https://api.boardgameatlas.com/api/search?name=${name}&pretty=true&client_id=${clientId}&year_published=${yearPublished}&min_players=${minPlayers}&max_players=${maxPlayers}&min_playtime=${minPlaytime}&max_playtime=${maxPlaytime}&min_age=${minAge}`);
         return data; 
     } catch (error) {
         console.log(error);
     }
  
-};
-
-async function searchByYear(year) {
-    try {
-        let clientId = "Yulk2IjBB9"
-        let {data}= await axios.get(`https://api.boardgameatlas.com/api/search?year_published=${year}&pretty=true&client_id=${clientId}`);
-        return data;
-    } catch (error){
-        console.log(error);
-    }
 };
 
 module.exports = router;
